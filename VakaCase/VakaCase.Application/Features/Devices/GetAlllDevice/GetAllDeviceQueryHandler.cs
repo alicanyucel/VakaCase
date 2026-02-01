f61b855c73a2a6828edbe3711d63c0891d3c6025
Ali Can Yücel <yucelalican@hotmail.com>
2026-02-01 13:12:11 +0300

    ok

--- VakaCase/VakaCase.Application/Features/Devices/GetAlllDevice/GetAllDeviceQueryHandler.cs
+++ VakaCase/VakaCase.Application/Features/Devices/GetAlllDevice/GetAllDeviceQueryHandler.cs
@@ -1,21 +1,16 @@
-﻿using TS.Result;
+﻿using MediatR;
+using TS.Result;
 using VakaCase.Domain.Entities;
 using VakaCase.Domain.Repositories;
 
 namespace VakaCase.Application.Features.Devices.GetAlllDevice;
 
-public class GetAllDevicesQueryHandler
+public sealed class GetAllDeviceQueryHandler(IDeviceRepository deviceRepository)
+    : IRequestHandler<GetAllDeviceQuery, Result<List<Device>>>
 {
-    private readonly IDeviceRepository _deviceRepository;
-
-    public GetAllDevicesQueryHandler(IDeviceRepository deviceRepository)
+    public Task<Result<List<Device>>> Handle(GetAllDeviceQuery request, CancellationToken cancellationToken)
     {
-        _deviceRepository = deviceRepository;
-    }
-
-    public async Task<Result<List<Device>>> Handle(GetAllDeviceQuery request, CancellationToken cancellationToken)
-    {
-        var devices = _deviceRepository.GetAll().Where(d => !d.IsDeleted).ToList();
-        return Result<List<Device>>.Succeed(devices);
+        var devices = deviceRepository.GetAll().Where(d => !d.IsDeleted).ToList();
+        return Task.FromResult(Result<List<Device>>.Succeed(devices));
     }
 }
